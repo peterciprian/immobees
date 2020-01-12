eee32702140e8bfc3e68d6b1db44761fc60c20e2
OPC <peterciprian@gmail.com>
2020-01-12 15:40:13 +0100

    firebase deploy

--- src/app/home/showroom/showroom.component.ts
+++ src/app/home/showroom/showroom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from 'src/app/core/models/accounts';
 
 @Component({
   selector: 'app-showroom',
@@ -7,7 +8,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowroomComponent implements OnInit {
 
-  constructor() { }
+  public accounts: Account[];
+  constructor() {
+    this.accounts = [
+      {
+        name: 'Réka',
+        age: 23,
+        spokenLanguages: ['hun', 'eng'],
+        school: 'bge',
+        subject: {
+          serviceType: 'keres',
+          address: { display: 'Budapest, XVIII. kerület' },
+        },
+        roomType: 'szobatárs',
+        moveIntoAt: new Date(Date.now()),
+        price: {
+          currency: 'huf',
+          min: 40000,
+          max: 60000,
+          billsIncluded: false
+        }
+      },
+      {
+        name: 'Réka',
+        age: 23,
+        spokenLanguages: ['hun', 'eng'],
+        school: 'bge',
+        subject: {
+          serviceType: 'keres',
+          address: { display: 'Budapest, XVIII. kerület' },
+        },
+        roomType: 'szobatárs',
+        moveIntoAt: new Date(Date.now()),
+        price: {
+          currency: 'huf',
+          min: 40000,
+          max: 60000,
+          billsIncluded: false
+        }
+      },
+      {
+        name: 'Réka',
+        age: 23,
+        spokenLanguages: ['hun', 'eng'],
+        school: 'bge',
+        subject: {
+          serviceType: 'keres',
+          address: { display: 'Budapest, XVIII. kerület' },
+        },
+        roomType: 'szobatárs',
+        moveIntoAt: new Date(Date.now()),
+        price: {
+          currency: 'huf',
+          min: 40000,
+          max: 60000,
+          billsIncluded: false
+        }
+      },
+      {
+        name: 'Réka',
+        age: 23,
+        spokenLanguages: ['hun', 'eng'],
+        school: 'bge',
+        subject: {
+          serviceType: 'keres',
+          address: { display: 'Budapest, XVIII. kerület' },
+        },
+        roomType: 'szobatárs',
+        moveIntoAt: new Date(Date.now()),
+        price: {
+          currency: 'huf',
+          min: 40000,
+          max: 60000,
+          billsIncluded: false
+        }
+      },
+      {
+        name: 'Réka',
+        age: 23,
+        spokenLanguages: ['hun', 'eng'],
+        school: 'bge',
+        subject: {
+          serviceType: 'keres',
+          address: { display: 'Budapest, XVIII. kerület' },
+        },
+        roomType: 'szobatárs',
+        moveIntoAt: new Date(Date.now()),
+        price: {
+          currency: 'huf',
+          min: 40000,
+          max: 60000,
+          billsIncluded: false
+        }
+      }
+    ];
+  }
 
   ngOnInit() {
   }
--- src/app/shared/immo-card/immo-card.component.ts
+++ src/app/shared/immo-card/immo-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Account } from 'src/app/core/models/accounts';
 
 @Component({
   selector: 'app-immo-card',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImmoCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() account: Account;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
