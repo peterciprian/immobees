891f3a0baff2ee2380cc5f5a476da2325b2c92b6
Oláh Péter Ciprián <peter.ciprian.olah@lechnerkozpont.hu>
2020-04-22 14:50:07 +0200

    register button fixed

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { FirebaseFirestoreService } from 'src/app/core/services/firebase-firestore.service';
 import { Account } from 'src/app/core/models/accounts';
+import { AuthModalService } from 'src/app/auth/auth-modal/auth-modal.service';
 
 @Component({
    selector: 'app-main',
@@ -17,6 +18,7 @@ export class MainComponent implements OnInit {
    constructor(
       private http: HttpClient,
       private firestore: FirebaseFirestoreService,
+      public authModalService: AuthModalService,
       public auth: AuthService, ) {
       this.firestore.$accounts.subscribe(acc => {
          this.highLight = acc.shift();
